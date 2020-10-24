62eb56c7ec9c25bc52bee398cb756d4bc8f74287
OUT-Arkhipov-DA <Akhipov.De.An@sberbank.ru>
2020-10-24 20:56:18 +0300

    refactor: delete comments

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -25,32 +25,3 @@ taskSchema.statics.toResponse = task => {
 const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
-
-// const uuid = require('uuid');
-
-// class Task {
-//   constructor({
-//     id = uuid(),
-//     title,
-//     order,
-//     description,
-//     userId,
-//     boardId,
-//     columnId
-//   } = {}) {
-//     this.id = id;
-//     this.title = title;
-//     this.order = order;
-//     this.description = description;
-//     this.userId = userId;
-//     this.boardId = boardId;
-//     this.columnId = columnId;
-//   }
-
-//   static toResponse(task) {
-//     const { id, title, order, description, userId, boardId, columnId } = task;
-//     return { id, title, order, description, userId, boardId, columnId };
-//   }
-// }
-
-// module.exports = Task;
